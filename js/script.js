a2928106027bc60557fd136e511e51467d61a2a2
nikita <erohinnikita100@gmail.com>
2021-03-08 16:12:14 +0300

    back to first solution

--- js/script.js
+++ js/script.js
@@ -1,43 +1,54 @@
 let button_right = document.getElementById('button_right');
 let button_left = document.getElementById('button_left');
-let massage_view_1 = document.getElementById('massage_view_1');
-let massage_view_2 = document.getElementById('massage_view_2');
-let massage_view_3 = document.getElementById('massage_view_3');
+// let massage_view = document.getElementById('massage_view');
+let desc_head = document.getElementById('desc_head');
+let desc_text = document.getElementById('desc_text');
+let img = document.getElementById('desc_img');
 
+
+function slide_1(){
+	desc_head.innerHTML = 'первый вид массажа';
+	desc_text.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quod recusandae, voluptates architecto quisquam suscipit libero, molestias nam eos adipisci, nemo illo illum totam at quasi provident laborum exercitationem quae.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quod recusandae, voluptates architecto quisquam suscipit libero, molestias nam eos adipisci, nemo illo illum totam at quasi provident laborum exercitationem quae.Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+	img.setAttribute('class', 'desc_img img_1');
+}
+
+function slide_2(){
+	desc_head.innerHTML = 'второй вид массажа';
+	desc_text.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quod recusandae, voluptates architecto quisquam suscipit libero, molestias nam eos adipisci, nemo illo illum totam at quasi provident laborum exercitto quisquam suscipit libero, molestias nam eos adipisci, nemo illo illum totam at quasi provident laborum exercitationem quae.Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+	img.setAttribute('class', 'desc_img img_2');
+}
+
+function slide_3(){
+	desc_head.innerHTML = 'третий вид массажа';
+	desc_text.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quod recusandae, voluptates architecto quisquam suscipit libero, molestias nam eoam eos adipisci, nemo illo illum totam at quasi provident laborum exercitationem quae.Lorem ipsum dolor sit amet consectetur adipisicing elit. '
+	img.setAttribute('class', 'desc_img img_3');
+}
 
 function right_click() {
-	if(massage_view_1.className === 'massage_view_1') {
-
-		massage_view_1.setAttribute('class', 'hidden');
-    massage_view_2.setAttribute('class', 'massage_view_2');
-
-	} else if (massage_view_2.className === 'massage_view_2') {
-
-		massage_view_2.setAttribute('class', 'hidden');
-    massage_view_3.setAttribute('class', 'massage_view_3');
-
-	} else if (massage_view_3.className === 'massage_view_3') {
-
-		massage_view_3.setAttribute('class', 'hidden');
-    massage_view_1.setAttribute('class', 'massage_view_1');
+	switch(img.className){
+		case 'desc_img img_1':
+		  slide_2();
+		  break;
+	  case 'desc_img img_2':
+	    slide_3();
+	    break;
+	  case 'desc_img img_3':
+	    slide_1();
+	    break;
 	}
 }
 
 function left_click() {
-  if(massage_view_1.className === 'massage_view_1') {
-
-		massage_view_1.setAttribute('class', 'hidden');
-		massage_view_3.setAttribute('class', 'massage_view_3');
-
-  } else if (massage_view_2.className === 'massage_view_2') {
-
-		massage_view_2.setAttribute('class', 'hidden');
-		massage_view_1.setAttribute('class', 'massage_view_1');
-
-  } else if (massage_view_3.className === 'massage_view_3') {
-
-		massage_view_3.setAttribute('class', 'hidden');
-		massage_view_2.setAttribute('class', 'massage_view_2');
+ switch(img.className){
+		case 'desc_img img_1':
+		  slide_3();
+		  break;
+	  case 'desc_img img_2':
+	    slide_1();
+	    break;
+	  case 'desc_img img_3':
+	    slide_2();
+	    break;
 	}
 }
 
