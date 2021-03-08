e196b2e0c0bba433b30a6a1fe121b47dee300c9c
nikita <erohinnikita100@gmail.com>
2021-03-08 17:50:11 +0300

    rigth_sl_anim

--- js/script.js
+++ js/script.js
@@ -1,24 +1,27 @@
 let button_right = document.getElementById('button_right');
 let button_left = document.getElementById('button_left');
-// let massage_view = document.getElementById('massage_view');
+let massage_view = document.getElementById('massage_view');
 let desc_head = document.getElementById('desc_head');
 let desc_text = document.getElementById('desc_text');
 let img = document.getElementById('desc_img');
 
 
 function slide_1(){
+	massage_view.setAttribute('class', 'massage_view slide_anim_1');
 	desc_head.innerHTML = 'первый вид массажа';
 	desc_text.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quod recusandae, voluptates architecto quisquam suscipit libero, molestias nam eos adipisci, nemo illo illum totam at quasi provident laborum exercitationem quae.Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quod recusandae, voluptates architecto quisquam suscipit libero, molestias nam eos adipisci, nemo illo illum totam at quasi provident laborum exercitationem quae.Lorem ipsum dolor sit amet consectetur adipisicing elit. '
 	img.setAttribute('class', 'desc_img img_1');
 }
 
 function slide_2(){
+	massage_view.setAttribute('class', 'massage_view slide_anim_2');
 	desc_head.innerHTML = 'второй вид массажа';
 	desc_text.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quod recusandae, voluptates architecto quisquam suscipit libero, molestias nam eos adipisci, nemo illo illum totam at quasi provident laborum exercitto quisquam suscipit libero, molestias nam eos adipisci, nemo illo illum totam at quasi provident laborum exercitationem quae.Lorem ipsum dolor sit amet consectetur adipisicing elit. '
 	img.setAttribute('class', 'desc_img img_2');
 }
 
 function slide_3(){
+	massage_view.setAttribute('class', 'massage_view slide_anim_3');
 	desc_head.innerHTML = 'третий вид массажа';
 	desc_text.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore quod recusandae, voluptates architecto quisquam suscipit libero, molestias nam eoam eos adipisci, nemo illo illum totam at quasi provident laborum exercitationem quae.Lorem ipsum dolor sit amet consectetur adipisicing elit. '
 	img.setAttribute('class', 'desc_img img_3');
